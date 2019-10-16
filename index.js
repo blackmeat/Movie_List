dbb9e8346fa0d3d3a49d89d313bf3d7522b1f0a7
Henry <joe8632782@gmail.com>
2019-10-16 11:44:36 +0800

    arrange code

--- index.js
+++ index.js
@@ -26,78 +26,80 @@
 	const moviePanel = document.querySelector("#movie-panel")
 	const footertag = document.querySelector(".card-footer")
 	const data = []
-	//const genres =[]
+	
 
 	// 取得API資料放進變數data
-		axios
-		.get(INDEX_URL)
-		.then( (response) => {
-				response.data.results.forEach(movie => {
-						data.push(movie)
-				})
+	axios
+	.get(INDEX_URL)
+	.then( (response) => {
+		response.data.results.forEach(movie => {
+			data.push(movie)
+			DisplayCard(data)
 		})
+	})
 
 
 	// 把電影分類列出來
-		const MovieType = document.querySelector("#movietype")
-		for (let i = 1 ; i < 20 ; i++) {
-			MovieType.innerHTML += `
-			<a class="list-group-item list-group-item-action" id="${MovieTypeData[i]}" data-toggle="list" href="#list" role="tab">${MovieTypeData[i]}</a>
-			`
-		}
+	const MovieType = document.querySelector("#movietype")
+	for (let i = 1 ; i < 20 ; i++) {
+		MovieType.innerHTML += `
+		<a class="list-group-item list-group-item-action" id="${MovieTypeData[i]}" data-toggle="list" href="#list" role="tab">${MovieTypeData[i]}</a>
+		`
+	}
 	
 
 	// 用card模式顯示電影清單
-		function DisplayCard (data) {
-			let movieContent = ""
-			data.forEach((item, index) => {
-			// 每部電影的genres對應到MovieTypeData資料，並放置在footer位置
+	function DisplayCard (data) {
+		let movieContent = ""
+		data.forEach((item, index) => {
+		// 每部電影的genres對應到MovieTypeData資料，並放置在footer位置
+		let genres = item.genres
+		let genresContent = ""
 
-				let genres = item.genres
-				let genresContent = ""
-				for(let i = 0 ; i < genres.length ; i++) {
-					genresContent += `
-						<p class="genres${genres[i]}">${MovieTypeData[genres[i]]}</p>
-					`
-				}
+		for(let i = 0 ; i < genres.length ; i++) {
+			genresContent += `
+			<p class="genres${genres[i]}">${MovieTypeData[genres[i]]}</p>
+			`
+			}
 
 			// 每部電影的海報及標題
-					movieContent += `
-					<div class="col-sm-3">
-						<div class="card mb-2">
-							<img class="card-img-top" src="${POSTER_URL}${item.image}" alt="Card image cap">
-								<div class="card-body movie-item-body">
-									<h6>${item.title}</h6>
+			movieContent += `
+				<div class="col-sm-3">
+					<div class="card mb-2">
+						<img class="card-img-top" src="${POSTER_URL}${item.image}" alt="Card image cap">
+							<div class="card-body movie-item-body">
+								<h6>${item.title}</h6>
+							</div>
+							<div class="card-footer">
+								<div class="row">
+									${genresContent}
 								</div>
-								<div class="card-footer">
-									<div class="row">
-										${genresContent}
-									</div>
-								</div>
-						</div>
+							</div>
 					</div>
-					`
-			})
-			moviePanel.innerHTML = movieContent
-		}
+				</div>
+				`
+		})
+		moviePanel.innerHTML = movieContent
+	}
+	
 	
 	// 當點擊分類時，從每一部電影裡找出genres包含在MovieTypeData
-		const movietypelist = document.querySelector("#movietypelist")
+	const movietypelist = document.querySelector("#movietypelist")
 		
-		movietypelist.addEventListener("click", function (event) {
-			for(let i = 1 ; i < 20 ;i++) {
-				if(event.target.matches(`#${MovieTypeData[i]}`)) {
-					// 顯示有包含該類型的電影
-					let dataKey =[]
-					dataKey.push(data.filter((item) => {return item.genres.includes(i)}))
-					console.log(...dataKey)
-					DisplayCard(...dataKey)
-					dataKey=[]
-					}
-				} 
-			})
+	movietypelist.addEventListener("click", function (event) {
+		for(let i = 1 ; i < 20 ;i++) {
+			if(event.target.matches(`#${MovieTypeData[i]}`)) {
+				// 顯示有包含該類型的電影
+				let dataKey =[]
+				dataKey.push(data.filter((item) => {return item.genres.includes(i)}))
+				console.log(...dataKey)
+				DisplayCard(...dataKey)
+				dataKey=[]
+			}
+		} 
+	})
 
-	// 
+	
   
 	
 })()
